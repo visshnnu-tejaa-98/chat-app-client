4d599535b7d9c3505c81f89afe833008688cfe67
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-03-07 12:31:38 +0530

    created join and chat pages

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+const Chat = () => {
+	return (
+		<div className='chat'>
+			<h1>Hello Chat Page</h1>
+		</div>
+	);
+};
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,9 @@
+const Join = () => {
+	return (
+		<div className='join'>
+			<h1>Hello Join Page</h1>
+		</div>
+	);
+};
+
+export default Join;
